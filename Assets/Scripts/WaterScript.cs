20ffa8f9588119da9f34daae72829960f7c45b02
Madina <madinabektayeva@gmail.com>
2018-01-24 22:30:46 +0900

    Decorations.prefab was created

---
+++ Assets/Scripts/WaterScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour {
+
+	Animator m_Animator;
+	float m_MySliderValue;
+
+	void Start()
+	{
+		m_Animator = gameObject.GetComponent<Animator>();
+	}
+
+	void OnGUI()
+	{
+		m_Animator.speed = 0.13F;
+	}
+}
