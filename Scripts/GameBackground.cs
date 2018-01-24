a70a54bf07a901a1813bef8489868cc62b5082dd
Madina <madinabektayeva@gmail.com>
2018-01-24 23:56:55 +0900

    Riffle added

---
+++ Scripts/GameBackground.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBackground : MonoBehaviour {
+
+	public Transform DecorationsPrefab;
+	Transform decTransform;
+	//bool menuMode = true;
+	bool gameMode = false;
+
+
+	void Start () {
+		
+	}
+		
+	void Update () {
+		
+		if (!gameMode) {
+			StartGame ();
+		}
+	}
+		
+	void StartGame(){
+		gameMode = true;
+		decTransform = Instantiate(DecorationsPrefab) as Transform;
+		decTransform.position = transform.position;
+	}
+
+	void DestroyGame(){
+		Destroy (decTransform.gameObject);
+		gameMode = false;
+	}
+
+
+	IEnumerator Example()
+	{
+		//	StartCoroutine (Example ());
+		StartGame ();
+		yield return new WaitForSeconds(5);
+		DestroyGame ();
+		yield return new WaitForSeconds(5);
+	}
+
+
+}
---
+++ Scripts/GunMoveScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMoveScript : MonoBehaviour {
+
+	public Transform MouseOutlinePrefab;
+	Transform MouseOutlineTransform;
+	float direction = 1;
+
+	void Start () {
+		MouseOutlineTransform = Instantiate(MouseOutlinePrefab) as Transform;
+	}
+
+	void Update () {
+		var mousePosition= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		MouseOutlineTransform.position = new Vector3(mousePosition.x,mousePosition.y, 0F);
+		var mouseX = mousePosition.x;
+		if (mouseX > 0) {
+			changeGunDirection (1);
+			this.transform.position = new Vector3 (mousePosition.x+0.5F, this.transform.position.y, 0F);
+		} else {
+			changeGunDirection (-1);
+			this.transform.position = new Vector3 (mousePosition.x-0.5F, this.transform.position.y, 0F);
+		}
+	}
+
+	void changeGunDirection(float d){
+		if(direction != d){
+			direction = d;
+			Vector3 newScale = this.transform.localScale;
+			newScale.x *= -1;
+			this.transform.localScale = newScale;
+		}
+	}
+		
+}
